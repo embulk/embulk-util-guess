3e5f2a7128c2822fa990fb88a2584658764c8073
Dai MIKURUBE <dmikurube@treasure-data.com>
2020-11-06 16:19:02 +0900

    Implement CharsetGuess

---
+++ src/main/java/org/embulk/util/guess/CharsetGuess.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2020 The Embulk project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.embulk.util.guess;
+
+import com.ibm.icu.text.CharsetDetector;
+import com.ibm.icu.text.CharsetMatch;
+import org.embulk.config.ConfigDiff;
+import org.embulk.spi.Buffer;
+import org.embulk.util.config.ConfigMapperFactory;
+
+/**
+ * Guesses a character set from {@link org.embulk.spi.Buffer}.
+ *
+ * <p>It reimplements {@code CharsetGuessPlugin} in {@code /embulk/guess/charset.rb}.
+ *
+ * @see <a href="https://github.com/embulk/embulk/blob/v0.10.19/embulk-core/src/main/ruby/embulk/guess/charset.rb">charset.rb</a>
+ */
+public final class CharsetGuess {
+    private CharsetGuess() {
+    }
+
+    public static CharsetGuess of() {
+        return new CharsetGuess();
+    }
+
+    /**
+     * Guesses a character set from {@link org.embulk.spi.Buffer}.
+     *
+     * @param sample  the byte sequence to be guessed
+     * @param configMapperFactory  {@link org.embulk.util.config.ConfigMapperFactory} for a new {@link org.embulk.config.ConfigDiff}
+     * @return {@link org.embulk.config.ConfigDiff} guessed
+     */
+    public ConfigDiff guess(final Buffer sample, final ConfigMapperFactory configMapperFactory) {
+        final CharsetDetector detector = new CharsetDetector();
+
+        final int sampleLength = sample.limit();
+        final byte[] sampleArray = new byte[sampleLength];
+        sample.getBytes(0, sampleArray, 0, sampleLength);
+
+        detector.setText(sampleArray);
+
+        final CharsetMatch bestMatch = detector.detect();
+
+        final ConfigDiff charset = configMapperFactory.newConfigDiff();
+        if (bestMatch.getConfidence() < 50) {
+            charset.set("charset", "UTF-8");
+        } else {
+            charset.set("charset", convertPredefined(bestMatch.getName()));
+        }
+
+        final ConfigDiff result = configMapperFactory.newConfigDiff();
+        result.setNested("parser", charset);
+        return result;
+    }
+
+    private static String convertPredefined(final String before) {
+        switch (before) {
+            // ISO-8859-1 means ASCII which is a subset of UTF-8 in most of cases
+            // due to lack of sample data set.
+            case "ISO-8859-1":
+                return "UTF-8";
+
+            // Shift_JIS is used almost only by Windows that uses "CP932" in fact.
+            // And "CP932" called by Microsoft actually means "MS932" in Java.
+            case "Shift_JIS":
+                return "MS932";
+
+            default:
+                return before;
+        }
+    }
+}
---
+++ src/test/java/org/embulk/util/guess/TestCharsetGuess.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020 The Embulk project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.embulk.util.guess;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import org.embulk.config.ConfigDiff;
+import org.embulk.util.config.ConfigMapperFactory;
+import org.junit.jupiter.api.Test;
+
+public class TestCharsetGuess {
+    @Test
+    public void test() {
+        assertCharset("UTF-8", "abc".getBytes(StandardCharsets.UTF_8));
+        assertCharset("MS932", "いろはにほへとちりぬるを".getBytes(Charset.forName("Shift_JIS")));
+        assertCharset("EUC-JP", "わかよたれそつねらなむ".getBytes(Charset.forName("EUC-JP")));
+    }
+
+    private static void assertCharset(final String expectedCharset, final byte[] sample) {
+        final ConfigDiff configDiff = CharsetGuess.of().guess(new FakeBufferImpl(sample), configMapperFactory);
+        assertEquals(expectedCharset, configDiff.getNested("parser").get(String.class, "charset"));
+    }
+
+    private static final ConfigMapperFactory configMapperFactory = ConfigMapperFactory.withDefault();
+}
